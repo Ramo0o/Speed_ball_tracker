6566a2a9e8748d9c4c6dad84469fb61b493a66e1
omar el hawary <omar.moelhawary@gmail.com>
2024-07-15 17:06:16 +0300

    Update tracker.js

--- public/tracker.js
+++ public/tracker.js
@@ -1,38 +1,29 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-submit.addEventListener("click",() => {
-    //turn them into objects 
-    let firstName = document.getElementById("first_name").value.trim().toLowerCase();
-    let lastName = document.getElementById("last_name").value.trim().toLowerCase();
-    let right = document.getElementById("right").value;
-    let left = document.getElementById("left").value;
-    let front = document.getElementById("front").value;
-    let back = document.getElementById("back").value;
-    let time = document.getElementById("time").value;
-    console.log(firstName,lastName,right,left,front,back,time)
-    fetch("https://speedballtracker-production.up.railway.app/api/insert_user",{
-        method:"POST",
-        body: JSON.stringify({
-            firstName : firstName,
-            lastName : lastName,
-            right :right,
-            left: left,
-            front: front,
-            back: back,
-            time: time
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+    submit.addEventListener("click",() => {
+        //turn them into objects 
+        let firstName = document.getElementById("first_name").value.trim().toLowerCase();
+        let lastName = document.getElementById("last_name").value.trim().toLowerCase();
+        let right = document.getElementById("right").value;
+        let left = document.getElementById("left").value;
+        let front = document.getElementById("front").value;
+        let back = document.getElementById("back").value;
+        let time = document.getElementById("time").value;
+        console.log(firstName,lastName,right,left,front,back,time)
+        fetch("http://localhost:3000/api/insert_user",{
+            method:"POST",
+            body: JSON.stringify({
+                firstName : firstName,
+                lastName : lastName,
+                right :right,
+                left: left,
+                front: front,
+                back: back,
+                time: time
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
     })
-    clearInput(document.getElementsByClassName("text_field"))
-    console.log("hi");
 })
-})
-
-function clearInput(textFields){
-    for (let i of textFields){
-        console.log(i)
-        i.value = "";
-    }
-}
